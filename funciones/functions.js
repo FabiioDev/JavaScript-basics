04cac74cd9adf6fe2e3b50de180501bc77213e00
fabiio <faboordo@gmail.com>
2024-05-21 14:55:25 -0600

    Ejercicios simples de funciones

---
+++ funciones/functions.js
@@ -0,0 +1,49 @@
+//Ejercicio 4
+export function suma(a, b) {
+    return a + b;
+}
+
+export function restar(a, b) {
+    return a - b;
+}
+
+
+//Ejercicio 3
+/* 
+const { format } = require('date-fns');
+
+function formatoFecha(fecha) {
+    return format(fecha, 'yyyy-MM-dd');
+}
+
+module.exports = formatoFecha
+*/
+
+/* //Ejercicio 2
+class Persona {
+    constructor(nombre, edad) {
+        this.nombre = nombre,
+            this.edad = edad;
+    }
+    saludo() {
+        console.log(`hola mi nombre es ${this.nombre} y tengo ${this.edad} años`);
+    }
+}
+
+module.exports = Persona
+ */
+
+/* //Ejercicio 1
+function agregar(a, b) {
+    return a + b;
+}
+
+function restar(a, b) {
+    return a - b;
+}
+
+module.exports = {
+    agregar,
+    restar
+}; 
+*/
---
+++ funciones/index.js
@@ -0,0 +1,43 @@
+//Ejercicio 4
+
+import { suma, restar } from './functions.js';
+const resultadoSuma = suma(10, 2);
+const resultadoResta = restar(8, 2);
+
+console.log(`El resultado de la suma es ${resultadoSuma}`);
+console.log(`El resultado de la resta es ${resultadoResta}`);
+
+
+
+//Ejercicio 3
+/*
+const fs = require('fs');
+const formatoFecha = require('./functions');
+
+
+const hoy = new Date();
+
+const formateo = formatoFecha(hoy);
+
+fs.writeFileSync('fecha.text', `La fecha de hoy es ${formateo}`);
+
+console.log('Le fecha se ha guardado');
+ */
+
+
+
+//Ejercicio 2
+/* 
+const persona = require('./functions')
+
+*/
+
+
+/* //Ejercicio 1
+const { agregar, restar } = require('./functions');
+
+let resultado = agregar(5, 3);
+console.log(`El resultado de la suma es: ${resultado}`);
+resultado = restar(10, 5);
+console.log(`El resultado de la resta: ${resultado}`); 
+*/
